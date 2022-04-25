aeed7adb3790aff65913b88ff85b04ed60373cda
anirudh <anirudhnikku22@gmail.com>
2022-04-25 12:00:44 +0530

    completed first draft

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,8 +2,22 @@ import React, { useEffect, useState } from 'react'
 import './List.css'
 import ListItem from './ListItem'
 import axios from "../axios"
+import YouTube from 'react-youtube';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
+
 function List({ title, url, smallCard }) {
-    const [movies, setMovie] = useState([])
+    const [movies, setMovie] = useState([]);
+    const [movieUrl, setMovieUrl] = useState(false)
+
+
+
+    const handleClose = () => {
+        setMovieUrl(false);
+    };
     useEffect(() => {
         axios.get(url).then((response) => {
             // console.log(response.data.results[0], `${imgUrl}${response.data.results[0].poster_path}`);
@@ -11,6 +25,14 @@ function List({ title, url, smallCard }) {
             console.log(response.data.results)
         })
     }, [])
+    const opts = {
+        height: '390',
+        width: '850',
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        },
+    };
     return (
         <div className='row'>
             <h2>{title}</h2>
@@ -19,11 +41,36 @@ function List({ title, url, smallCard }) {
                     movies.map((singleMovie) => {
                         // console.log(singleMovie)
                         return (
-                            <ListItem movie={singleMovie} smallCard={smallCard} />
+                            <ListItem movie={singleMovie} smallCard={smallCard} setMovieUrl={setMovieUrl} />
                         )
                     })
                 }
             </div>
+            {
+                movieUrl &&
+                <Dialog
+                    PaperProps={{
+                        style: {
+                            backgroundColor: '#0b0b0b'
+                        }
+                    }}
+                    open={movieUrl}
+                    fullWidth={true}
+                    maxWidth="md"
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        <h2 style={{ color: "white" }}>{movieUrl.name}</h2>
+                    </DialogTitle>
+                    <DialogContent>
+                        <YouTube videoId={movieUrl.key} opts={opts} />
+                    </DialogContent>
+
+                </Dialog>
+
+            }
         </div>
     )
 }
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import "./ListItem.css";
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import { Add } from "@mui/icons-material";
-import { imgUrl } from "../constants/constant";
+import { API_KEY, baseUrl, imgUrl } from "../constants/constant";
+import axios from "axios";
 
 
-function ListItem({ movie, smallCard }) {
+function ListItem({ movie, smallCard, setMovieUrl }) {
+    const fetchTrailer = () => {
+        axios.get(`${baseUrl}/movie/${movie.id}/videos?api_key=${API_KEY}`).then((response) => {
+            console.log(response.data.results)
+            setMovieUrl(response.data.results[0])
+        })
+    }
 
     return (
-        <div className={smallCard ? 'listItem small' : "listItem"} style={{ background: `url(${imgUrl + movie.backdrop_path})`, backgroundSize: 'cover' }}>
+        <div className={smallCard ? 'listItem small' : "listItem"} style={{ background: `url(${imgUrl + movie.backdrop_path})`, backgroundSize: 'cover' }} >
             <div className="icon-group">
                 {movie.title}
                 <div className="icons">
-                    <PlayArrowIcon className="icon" />
+                    <PlayArrowIcon className="icon" onClick={fetchTrailer} />
                     <Add className="icon" />
                 </div>
             </div>
